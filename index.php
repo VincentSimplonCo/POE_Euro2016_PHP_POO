08b3aa2ab95ee107dcd030c73b4c8cbeb8608459
VincentSimplonCo <vincentperche.lyon@gmail.com>
2016-06-23 10:34:59 +0200

    View for Teams and Groups: OK

--- index.php
+++ index.php
@@ -7,17 +7,37 @@
   </head>
   <body>
 
-    <h1 id="title">Euro 2016</h1>
+    <!-- <h1 id="title">Euro 2016</h1> -->
 
 
     <?php
 
       require "get_json.php";
 
-      echo "<pre>";
-      print_r($competition);
-      echo "</pre>";
+      $entete = "<h1 id='title'>".($competition->name)."</h1>";
+      echo $entete;
+
+      // function viewGroups(){
+      $affichageGroupes;
+
+        for ($compteurGroupe = 0; $compteurGroupe < count($competition->groups); $compteurGroupe++) {
+          $groupes = $competition->groups[$compteurGroupe]->id;
+          $affichageGroupes .= "<div class='conteneur'>"."<h2 class='groupes'>"."Groupe: ".$groupes."</h2>";
+
+          for ($compteurEquipe = 0; $compteurEquipe < count($competition->groups[$compteurGroupe]->teams) ; $compteurEquipe++) {
+            $equipes = $competition->groups[$compteurGroupe]->teams[$compteurEquipe];
+            $affichageGroupes .= "<li class='equipes'>".$equipes."</li>";
+          }
+          $affichageGroupes .= "</div>";
+        }
+
+        echo $affichageGroupes;
+      // }
+
+      // viewGroups();
     ?>
+
+    <div>
 
   </body>
 </html>
